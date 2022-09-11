65f787caa208170117db8b669c44d2f2edee3abf
Jack Finnis <finnis.j@etoncollege.org.uk>
2022-09-11 10:57:01 +0100

    Initial Commit

---
+++ Paddle/PaddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaddleApp.swift
+//  Paddle
+//
+//  Created by Jack Finnis on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PaddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
